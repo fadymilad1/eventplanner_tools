285d52aab41038761ae31da3571ce1b04ecfc2c0
Your Name <your.email@example.com>
2025-12-26 23:16:31 +0200

    Update API URL for OpenShift

--- event-frontend/src/environments/environment.prod.ts
+++ event-frontend/src/environments/environment.prod.ts
@@ -1,7 +1,5 @@
 export const environment = {
   production: true,
-  // Backend URL - accessible from browser
-  // In Docker, backend is accessible via the host machine's localhost
-  apiUrl: 'http://localhost:5000/api'
+  apiUrl: 'https://event-backend-amrm72004-dev.apps.rm3.7wse.p1.openshiftapps.com/api'
 };
 
